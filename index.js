ebf00790900cb764c8882ef41c9cbb887ae4955c
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-10-12 16:51:33 -0600

    Put question list in the index.js. Set up license functions in generateMarkDown.js

--- index.js
+++ index.js
@@ -1,26 +1,78 @@
 // TODO: Include packages needed for this application
 const genMarkdown = require('./utils/generateMarkdown');
-
-// TODO: Create an array of questions for user input
-const questions = [
-    projectTitle,
-    description,
-    installInst,
-    usage,
-    license,
-    contributing,
-    tests,
-    gitHubUsername,
-    email
-];
-
-console.log(questions);
+const inquirer = require('inquirer')
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt([{
+    type: 'input',
+    name: 'projectTitle',
+    message: 'What is the name of your project/app?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "Provide a basic descriotion of your project:"
+  },
+  {
+    type: 'input',
+    name: 'installInst',
+    message: 'How can the viewer install the code?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How is this code used?'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What licesnse does your project use?',
+    //FIX THE CHOICES SO THEY MATCH THE EXACT LOOK OF IN THE GITHUB BADGES URL - https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+    choices: [
+      'None', 
+      'Apache_2.0', 
+      'GNU General Public v3.0', 
+      'MIT',
+      'BSD 2-Clause "Simplified"',
+      'BSD 3-Clause "New" or "Revised"',
+      'Boost Software',
+      'Creative Commons Zero v1.0 Universal',
+      'Eclipse Public 2.0',
+      'GNU Affero General Public v3.0',
+      'GNU General Public v2.0',
+      'GNU Lesser General Public v2.1',
+      'Mozilla Public 2.0',
+      'The Unlicense'
+    ]
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can individuals contribute (if they are able to)?'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What tests if any did you run?'
+  }, 
+  {
+    type: 'input',
+    name: 'gitHubUsername',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your contact email?'
+  }
+  ])
+  .then((response) => console.log(genMarkdown(response))) //This to test inputs were recorded.
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,85 +2,36 @@ const inquirer = require('inquirer')
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "None") {
+    return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+}
+  
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "None") {
+    return `[License](#license)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-inquirer
-  .prompt([{
-    type: 'input',
-    name: 'projectTitle',
-    message: 'What is the name of your project/app?'
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: "Provide a basic descriotion of your project:"
-  },
-  {
-    type: 'input',
-    name: 'installInst',
-    message: 'How can the viewer install the code?'
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'How is this code used?'
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'What licesnse does your project use?',
-    choices: [
-      'None', 
-      'Apache License 2.0', 
-      'GNU General Public Licesnse v3.0', 
-      'MIT License',
-      'BSD 2-Clause "Simplified" License',
-      'BSD 3-Clause "New" or "Revised" License',
-      'Boost Software License',
-      'Creative Commons Zero v1.0 Universal',
-      'Eclipse Public License 2.0',
-      'GNU Affero General Public License v3.0',
-      'GNU General Public License v2.0',
-      'GNU Lesser General Public Licesnse v2.1',
-      'Mozilla Public License 2.0',
-      'The Unlicense'
-    ]
-  },
-  {
-    type: 'input',
-    name: 'contributing',
-    message: 'How can individuals contribute (if they are able to)?'
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'What tests if any did you run?'
-  }, 
-  {
-    type: 'input',
-    name: 'gitHubUsername',
-    message: 'What is your GitHub username?'
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your contact email?'
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `##License: ${renderLicenseBadge(license)}`
   }
-  ])
-  .then((response) => console.log(response)) //This to test inputs were recorded.
+}
 
 
+// TODO: Create a function to generate markdown for README //BUILD OUT A README HERE
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+
+  ${renderLicenseSection(data.license)}
+
 
 `;
 }
